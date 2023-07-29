6381825aa66163f85830465d50aac166da0e0aba
flow <flowbehappy@gmail.com>
2023-07-29 20:12:10 +0800

    fix connect token, parse room name correctly

--- lib/utils/tidb.dart
+++ lib/utils/tidb.dart
@@ -1,3 +1,26 @@
+import 'package:args/args.dart';
+
+ArgParser tokenParser = createTokenParser();
+
+ArgParser createTokenParser() {
+  var p = ArgParser();
+  p.addOption("host", abbr: "h");
+  p.addOption("port", abbr: "P");
+  p.addOption("user", abbr: "u");
+  p.addOption("password", abbr: "p");
+  p.addOption("room");
+  p.addOption("room_name");
+
+  // Options we don't use yet. But we has to define too. Otherwise parser will complain
+  p.addOption("connect-timeout");
+  p.addOption("database", abbr: "D");
+  p.addOption("ssl-mode");
+  p.addOption("ssl-ca");
+
+  p.allowsAnything;
+  return p;
+}
+
 String toConnectionToken(
   String host,
   int port,
@@ -6,62 +29,85 @@ String toConnectionToken(
   String roomId,
   String roomName,
 ) {
-  return "mysql -u '$userName' -h $host -P 4000 -p$password --room $roomId --room_name $roomName";
+  return "mysql -u '$userName' -h '$host' -P $port -p$password --room '$roomId' --room_name '$roomName'";
 }
 
 // Return (host, port, user, password, tableId).
 // If port == 0, means "host" is the error message.
 (String, int, String, String, String, String) parseTiDBConnectionToken(
-    String text) {
-  if (text.isEmpty) {
+    String token) {
+  if (token.isEmpty) {
     return ("", 0, "", "", "", "");
   }
-
-  text = text.replaceFirst(" -p", " -p ");
-  final options = text.split(" ");
-  var nextOpts = List.from(options);
-  nextOpts.removeAt(0);
-  nextOpts.add("");
-  String user = "";
-  String host = "";
-  int port = 0;
-  String password = "";
-  String msgRoom = "";
-  String msgRoomName = "";
-
+  // token =
+  // "mysql --connect-timeout 15 -u '3DQS6CX9AX9qY51.root' -h gateway01.eu-central-1.prod.aws.tidbcloud.com -P 4000 -D test --ssl-mode=VERIFY_IDENTITY --ssl-ca=/etc/ssl/cert.pem -piAaVmmI4dypKbEgY";
   try {
-    for (int i = 0; i < options.length; i += 1) {
-      final opt = options[i];
-      final nextOpt = nextOpts[i];
-      switch (opt) {
-        case "-u":
-        case "--user":
-          user = nextOpt.replaceAll("'", "");
-          user = user.replaceAll('"', "");
-          user = user.replaceAll("'", "");
-          break;
-        case "-h":
-        case "--host":
-          host = nextOpt;
-          break;
-        case "-P":
-        case "--port":
-          port = int.parse(nextOpt);
-          break;
-        case "-p":
-        case "--password":
-          password = nextOpt;
-          break;
-        case "--room":
-          msgRoom = nextOpt;
-        case "--room_name":
-          msgRoomName = nextOpt;
-        default:
-      }
-    }
+    final tokens = tokenize(token);
+    final r = tokenParser.parse(tokens);
+    final host = r["host"];
+    final port = int.parse(r["port"]);
+    final user = r["user"];
+    final password = r["password"];
+    final roomId = r["room"] ?? "";
+    final roomName = r["room_name"] ?? "";
 
-    return (host, port, user, password, msgRoom, msgRoomName);
+    return (host, port, user, password, roomId, roomName);
   } catch (e) {
     return (e.toString(), 0, "", "", "", "");
   }
+}
+
+List<String> tokenize(String input) {
+  if (input.isEmpty) {
+    //no command? no string
+    return [];
+  }
+
+  var result = List<String>.empty(growable: true);
+
+  var current = "";
+
+  String? inQuote;
+  bool lastTokenHasBeenQuoted = false;
+
+  for (int index = 0; index < input.length; index++) {
+    final token = input[index];
+
+    if (inQuote != null) {
+      if (token == inQuote) {
+        lastTokenHasBeenQuoted = true;
+        inQuote = null;
+      } else {
+        current += token;
+      }
+    } else {
+      switch (token) {
+        case "'": // '
+        case '"': // ""
+          inQuote = token;
+          continue;
+
+        case " ": // space
+          if (lastTokenHasBeenQuoted || current.isNotEmpty) {
+            result.add(current);
+            current = "";
+          }
+          break;
+
+        default:
+          current += token;
+          lastTokenHasBeenQuoted = false;
+      }
+    }
+  }
+
+  if (lastTokenHasBeenQuoted || current.isNotEmpty) {
+    result.add(current);
+  }
+
+  if (inQuote != null) {
+    throw Exception("Unbalanced quote $inQuote in input: $input");
+  }
+
+  return result;
 }
