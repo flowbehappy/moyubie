1f9c564d444131d93b8221eede292d3ec371ea36
flow <flowbehappy@gmail.com>
2023-08-06 19:35:55 +0800

    fix tab color of dark mode

--- lib/data/color.dart
+++ lib/data/color.dart
@@ -13,12 +13,10 @@ const List<Color> _colors = [
   Color(0xfff14738),
   Color(0xfffe5037),
   Color(0xffef7b77),
-
   Color(0xffdd4d11),
   Color(0xffe76714),
   Color(0xffed7616),
   Color(0xfff38618),
-
   Color(0xff920059),
   Color(0xffb90060),
   Color(0xffce0063),
@@ -39,7 +37,8 @@ const FlexSchemeData moyubieSchemeData = FlexSchemeData(
     swapOnMaterial3: true,
   ),
   dark: FlexSchemeColor(
-    primary: Color(0xFF4E597D),
+    // primary: Color(0xFF4E597D),
+    primary: Color.fromARGB(255, 42, 42, 42),
     primaryContainer: Color(0xFF202541),
     secondary: Color(0xFF4BA390),
     secondaryContainer: Color(0xFF0B5341),
